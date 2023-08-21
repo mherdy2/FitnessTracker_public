6b03931d6b3763cddeedd277b6c9982d8bb8eb49
mherdy2 <mherdy_2@hotmail.com>
2023-08-20 19:29:11 -0700

    initial commit

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
+const connectionString = process.env.DATABASE_URL || 'postgres://wasdzexa:PQwtBgX4azuP_vqJmIukcp_RJ39IOxTM@bubble.db.elephantsql.com/wasdzexa';
 
 const client = new Client({
   connectionString,
